c2cf74cc6721eebe924eca73651bdfd9d9e7bb49
Rizky Ramadhana <rizkyramadhana26@gmail.com>
2022-11-29 13:44:08 +0700

    feat: add checking APIKey

--- soap/src/main/java/binotifysoap/services/Subscription.java
+++ soap/src/main/java/binotifysoap/services/Subscription.java
@@ -17,25 +17,41 @@ public class Subscription {
   @Resource
   WebServiceContext wsContext;
 
+  MessageContext msgx = this.wsContext.getMessageContext();
+  HttpExchange exchange = (HttpExchange)this.msgx.get("com.sun.xml.ws.http.exchange");
+
   private void log(String desc, String endpoint){
-    try{
-      MessageContext msgx = this.wsContext.getMessageContext();
-      HttpExchange exchange = (HttpExchange)msgx.get("com.sun.xml.ws.http.exchange");
-      System.out.println("Client IP = " + exchange.getRemoteAddress()); 
+    try{      
+      System.out.println("Client IP = " + this.exchange.getRemoteAddress()); 
       DBUtil db = new DBUtil();
       System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date()));
-      db.create(String.format("INSERT INTO Logging(description,IP,endpoint,requested_at) VALUES ('%s','%s','%s','%s')",desc,exchange.getRemoteAddress(),endpoint, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date())));
+      db.create(String.format("INSERT INTO Logging(description,IP,endpoint,requested_at) VALUES ('%s','%s','%s','%s')",desc,this.exchange.getRemoteAddress(),endpoint, new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date())));
     }catch(Exception e){
       e.printStackTrace();
     }
   }
 
+  private boolean check(){
+    String apiKey = exchange.getRequestHeaders().getFirst("apiKey");
+    String source = exchange.getRequestHeaders().getFirst("source");
+
+    //bandingkan apikey dan source
+    if( true ){
+      return true;
+    }else{
+      return false;
+    }
+    
+  }
+
   @WebMethod
   public void add(int creator_id, int subscriber_id){
     try{
-      this.log(String.format("menambahkan permintaan subscription baru untuk creator_id=%d dan subscriber_id =%d",creator_id,subscriber_id),"localhost/soap/subscription");
-      DBUtil db = new DBUtil();
-      db.create(String.format("INSERT INTO Subscription(creator_id,subscriber_id) VALUES (%d,%d)",creator_id,subscriber_id));
+      if(check()){
+        this.log(String.format("menambahkan permintaan subscription baru untuk creator_id=%d dan subscriber_id =%d",creator_id,subscriber_id),"localhost/soap/subscription");
+        DBUtil db = new DBUtil();
+        db.create(String.format("INSERT INTO Subscription(creator_id,subscriber_id) VALUES (%d,%d)",creator_id,subscriber_id));
+      }
     }catch(Exception e){
       e.printStackTrace();
     }
@@ -44,9 +60,11 @@ public class Subscription {
   @WebMethod
   public void setStatus(int creator_id, int subscriber_id, String status){
     try{
-      this.log(String.format("mengubah status subscription creator_id=%d dan subscriber_id =%d",creator_id,subscriber_id),"localhost/soap/subscription");
-      DBUtil db = new DBUtil();
-      db.create(String.format("UPDATE Subscription SET status='%s' WHERE creator_id=%d AND subscriber_id=%d",status,creator_id,subscriber_id));
+      if(check()){
+        this.log(String.format("mengubah status subscription creator_id=%d dan subscriber_id =%d",creator_id,subscriber_id),"localhost/soap/subscription");
+        DBUtil db = new DBUtil();
+        db.create(String.format("UPDATE Subscription SET status='%s' WHERE creator_id=%d AND subscriber_id=%d",status,creator_id,subscriber_id));
+      }
     }catch(Exception e){
       e.printStackTrace();
     }
