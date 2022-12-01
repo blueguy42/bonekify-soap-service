e72a06366ec128475dbc3d9a825a617beb4ead29
Ahmad Alfani Handoyo <ahmadalfanihandoyo@hotmail.com>
2022-12-02 04:27:21 +0700

    add: realistic dump

--- dump/dump.sql
+++ dump/dump.sql
@@ -25,9 +25,30 @@ CREATE TABLE IF NOT EXISTS `Subscription` (
   PRIMARY KEY (`creator_id`,`subscriber_id`)
 );
 
-INSERT INTO `Subscription` VALUES
-(2,2,'PENDING'),
-(3,2,'ACCEPTED'),
-(4,2,'REJECTED');
+INSERT INTO `Subscription` (`creator_id`, `subscriber_id`, `status`) VALUES
+(3,5,'ACCEPTED'),
+(4,6,'PENDING'),
+(5,7,'PENDING'),
+(6,8,'PENDING'),
+(7,9,'PENDING'),
+(8,10,'ACCEPTED'),
+(9,11,'PENDING'),
+(10,12,'PENDING'),
+(3,13,'PENDING'),
+(4,14,'PENDING'),
+(5,15,'ACCEPTED'),
+(6,16,'REJECTED'),
+(7,5,'PENDING'),
+(8,6,'PENDING'),
+(9,7,'PENDING'),
+(10,8,'ACCEPTED'),
+(3,9,'PENDING'),
+(4,10,'PENDING'),
+(5,11,'PENDING'),
+(6,12,'PENDING'),
+(7,13,'ACCEPTED'),
+(8,14,'PENDING'),
+(9,15,'PENDING'),
+(10,16,'PENDING');
 
 COMMIT;
